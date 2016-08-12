346677abcf91c065ecb168ac200d51cbc18fc608
gpapadog <gpapadogeorgou@fas.harvard.edu>
2016-08-12 14:23:21 -0400

    Upload of distance functions

---
+++ R/distance_functions.R
@@ -0,0 +1,33 @@
+StandDist <- function(x) {
+  # Function that takes in a matrix and returns the equivalent
+  # matrix with elements between 0 and 1, by subtracting the
+  # minimum and dividing by the range.
+  #
+  # Args:
+  #  x: Matrix of any dimensions. There should be at least two
+  #     different elements in x. Numeric.
+  #
+  # Returns:
+  #  Matrix of the same dimensions as x with entries between
+  #  0 and 1.
+  
+  standx <- (x - min(x)) / (max(x) - min(x))
+  return(standx)
+}
+
+EmpCDF <- function(x) {
+  # Function that takes in a matrix and returns the empirical
+  # cdf of every entry of the matrix.
+  #
+  # Args:
+  #  x: Matrix of any dimensions. There should be at least two
+  #     different elements in x. Numeric.
+  #
+  # Returns:
+  #  Matrix of the same dimensions as x with empirical cdf.
+  
+  require(stats)
+  y <- ecdf(x)(x)
+  y <- matrix(y, nrow = nrow(x), ncol = ncol(x))
+  return(y)
+}
