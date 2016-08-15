9c56a1bff06c8925b962c807fa390b58193eb687
gpapadog <gpapadogeorgou@fas.harvard.edu>
2016-08-15 16:58:45 -0400

    Examples

--- R/distance_functions.R
+++ R/distance_functions.R
@@ -1,31 +1,45 @@
+#' Standardizing a matrix to be between 0 and 1.
+#'
+#' Takes in a matrix and subtract the minimum element and divides by the range
+#' to return a matrix with elements between 0 and 1.
+#'
+#' @param x Matrix to be standardized. There should be at least two different
+#' entries in x.
+#'
+#' @return A matrix of the same dimensions as the one given.
+#' 
+#' @examples
+#' set.seed(1)
+#' D <- matrix(rexp(800, rate = 0.5), 20, 40)
+#' Dnew <- StandDist(D)
+#' hist(D)
+#' hist(Dnew)
+#' 
+#' @export
 StandDist <- function(x) {
-  # Function that takes in a matrix and returns the equivalent
-  # matrix with elements between 0 and 1, by subtracting the
-  # minimum and dividing by the range.
-  #
-  # Args:
-  #  x: Matrix of any dimensions. There should be at least two
-  #     different elements in x. Numeric.
-  #
-  # Returns:
-  #  Matrix of the same dimensions as x with entries between
-  #  0 and 1.
-  
   standx <- (x - min(x)) / (max(x) - min(x))
   return(standx)
 }
 
+
+#' Standardizing a matrix to be between 0 and 1 using the empirical CDF.
+#'
+#' Takes in a matrix and returns a matrix of the same dimension with entries
+#' equal to the corresponding empirical cdf value.
+#'
+#' @param x Matrix to be standardized.
+#'
+#' @return A matrix of the same dimensions as the one given.
+#' 
+#' @examples
+#' set.seed(1)
+#' D <- matrix(rexp(800, rate = 0.5), 20, 40)
+#' Dnew <- EmpCDF(D)
+#' hist(D)
+#' hist(Dnew)
+#' 
+#' @export
 EmpCDF <- function(x) {
-  # Function that takes in a matrix and returns the empirical
-  # cdf of every entry of the matrix.
-  #
-  # Args:
-  #  x: Matrix of any dimensions. There should be at least two
-  #     different elements in x. Numeric.
-  #
-  # Returns:
-  #  Matrix of the same dimensions as x with empirical cdf.
-  
   y <- stats::ecdf(x)(x)
   y <- matrix(y, nrow = nrow(x), ncol = ncol(x))
   return(y)
